0c93de66b6d2143a3e080e7497b6299a517a2b8c
SammyKipp <71403313+SammyKipp@users.noreply.github.com>
2021-11-16 14:10:10 -0500

    Update server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+var app = express()
+app.get('/', function(req, res){
+  res.send("Hello World")
+})
+var server = app.listen(8000, function(){
+  var host = server.address().address
+  var port = server.address().port
+  console.log("listening")
+})
