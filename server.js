9b5e115750b5804c0a8f7702d2770e5c4c74aaf6
SammyKipp <71403313+SammyKipp@users.noreply.github.com>
2021-11-17 10:14:36 -0500

    added an ejs template

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const path = require('path');
+
 var app = express()
+
+app.use(express.urlencoded({ extended: true}))
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
 app.get('/', function(req, res){
-  res.send("Hello World")
+  res.render('organizer', {
+
+  })
 })
 var server = app.listen(8000, function(){
   var host = server.address().address
