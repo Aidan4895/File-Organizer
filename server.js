1ff7233095b130d4c91a3a394a5728c62a138dde
SammyKipp <71403313+SammyKipp@users.noreply.github.com>
2021-11-17 10:47:32 -0500

    why is it just spinning?!

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ app.get('/', function(req, res){
 
   })
 })
+app.post('/action_page.php', function(req, res){
+  if (req.body.file){
+    console.log('working')
+    res.render('organizer2', {
+      yell: `You submitted: ${req.body.file}`
+    })
+  }
+})
 var server = app.listen(8000, function(){
   var host = server.address().address
   var port = server.address().port
