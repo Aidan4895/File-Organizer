8cdb09b8313233fcc6ae4c7112291b69b13822cf
Jlara2005 <90395076+Jlara2005@users.noreply.github.com>
2021-11-18 14:42:29 -0500

    prototype

--- server.js
+++ server.js
@@ -11,16 +11,30 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', function(req, res){
+  var rawfile = fs.readFileSync('files.json')
+  var file = JSON.parse(rawfile)
   res.render('organizer', {
-
+    filelist: file.files
   })
 })
 app.post('/action_page.php', upload.single('filename'), function (req, res, next) {
   console.log(req.file);
+  var rawfile = fs.readFileSync('files.json')
+  var file = JSON.parse(rawfile)
+  console.log(file)
+  file["files"].push(req.file.originalname)
+
+  var sendfile = JSON.stringify(file)
+  console.log(sendfile)
+  fs.writeFile("files.json",sendfile,"utf8",function(){
+  console.log("file sent")
+  })
   fs.rename('uploads/' + req.file.filename, 'uploads/' + req.file.originalname, function(){
     console.log("Rewrote File")
   })
-  res.render('organizer')
+  res.render('organizer', {
+    filelist: file.files
+  })
 })
 /*app.post('/action_page.php', function(req, res){
   console.log(req.body.file);
